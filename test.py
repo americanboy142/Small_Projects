11f6265a3a107a9b67db9eccc7e3d85431237921
jack phreaner <jphreaner142@gmail.como>
2023-03-11 12:42:03 -0800

    QR fact

--- test.py
+++ test.py
@@ -17,8 +17,68 @@ x_prev = [5,0,0]
 print(thin)
 for i in range(len(x)):
     print(x[i]-x_prev[i]) """
-print(len(x))
+""" print(len(x))
 for i in range(3,3):
-    print(i)
+    print(i) """
 #for i in range(1):
     #print(i)
+
+
+a = [1,2,3]
+
+def two_norm(U):
+    return (sum(U[i]**2 for i in range(len(U))))**(1/2)
+
+thing = []
+for i in range(len(a)):
+    thing.append(a[i]/two_norm(a))
+print(thing)
+
+a = [[1,2,3],[1,2,3],[1,2,3]]
+b = [[1,2,3],[1,2,3],[1,2,3]]
+thing = []
+final = []
+for i in range(len(a)):
+    thing = []
+    for deal in range(len(a[i])):
+        thing.append(a[i][deal]+b[i][deal])
+    final.append(thing)
+if type(a) == list:
+    print("hi")
+
+import numpy as np
+
+a = np.array([[1,2],[1,2]])
+b = np.array([[1,2],[1,2]])
+
+#print(a[0])
+deal = np.array([[]])
+
+#deal = np.concatenate((deal,[[1,2,3]]))
+print("deal: ",deal)
+
+#deal = np.concatenate((deal,[[4,5,6]]), axis=0)
+print("deal:",deal)
+
+c = np.array([1,2])
+print(5*c)
+
+poo = [[1,2,3],[1,2,3]]
+
+thing1 = np.array([])
+thing2 = np.array([])
+
+poo.append([1,2,3])
+print(poo)
+
+shoot = np.add(poo[0],poo[1])
+print("shoot: ",shoot)
+
+thing1 = np.concatenate((thing1,poo[0]))
+thing2 = np.concatenate((thing2,poo[1]))
+print(thing1-thing2)
+for i in poo:
+    thing = np.array([])
+    thing = np.append(thing,i)
+    print(thing)
+
